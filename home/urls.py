efce06402feb02a97310561c456782f1a5b514e6
hadiagha07 <hadigholamii077@gmail.com>
2025-02-04 18:34:52 +0330

    fix the bugs of OTP

--- home/urls.py
+++ home/urls.py
@@ -19,7 +19,9 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('signup/', SignUpView.as_view(), name='signup'),
+    path('send-otp/', RequestOTPView.as_view(), name='send_otp'),  # ارسال کد تأیید
+    path('verify-otp/', VerifyOTPView.as_view(), name='verify_otp'),  # تأیید کد
+    path('sign-up/', SignUpView.as_view(), name='sign_up'),  # ثبت‌نام کاربر
     path('login/', LoginView.as_view(), name='login'),
     path('active-question/', ActiveQuestionView.as_view(), name='active-question'),
     path('submit-response/', SubmitResponseView.as_view(), name='submit-response'),
